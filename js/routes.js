24f40fe4195a3c403b0f3b421d1ee13b88584c2d
alfiantaufani <alfiantaufani@gmail.com>
2022-08-22 22:12:23 +0700

    first commit

---
+++ js/routes.js
@@ -0,0 +1,11 @@
+var routes = [
+  { path: '/home/', componentUrl: 'pages/home.html' },
+  { path: '/login/', componentUrl: 'pages/login.html' },
+  { path: '/profil/', componentUrl: 'pages/profil.html' },
+  { path: '/dtpasien/', componentUrl: 'pages/dtpasien.html' },
+  { path: '/dtukur/', componentUrl: 'pages/dtukur.html' },
+  { path: '/bb/', componentUrl: 'pages/bb.html' },
+  { path: '/tb/', componentUrl: 'pages/tb.html' },
+  { path: '/daftar/', componentUrl: 'pages/daftar.html' },
+  { path: '(.*)', url: 'pages/404.html', }
+];
